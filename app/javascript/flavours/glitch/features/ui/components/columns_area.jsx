6bd86fede0a9171cf47e1dcf363bbcbd4e2c3068
Claire <claire.github-309c@sitedethib.com>
2024-09-17 05:19:42 +0200

    [Glitch] Increase no-gap-breakpoint by 32px to fix overflow
    Port d2f907507857fb0d256c45dda34403ae1fe95cd8 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/features/ui/components/columns_area.jsx
+++ app/javascript/flavours/glitch/features/ui/components/columns_area.jsx
@@ -64,8 +64,8 @@ export default class ColumnsArea extends ImmutablePureComponent {
     openSettings: PropTypes.func,
   };
 
-  // Corresponds to (max-width: $no-gap-breakpoint + 285px - 1px) in SCSS
-  mediaQuery = 'matchMedia' in window && window.matchMedia('(max-width: 1174px)');
+  // Corresponds to (max-width: $no-gap-breakpoint - 1px) in SCSS
+  mediaQuery = 'matchMedia' in window && window.matchMedia('(max-width: 1206px)');
 
   state = {
     renderComposePanel: !(this.mediaQuery && this.mediaQuery.matches),
