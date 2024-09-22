4d754935a98aa9970f2d90db81c83f77be6ca654
Claire <claire.github-309c@sitedethib.com>
2024-09-22 20:21:46 +0200

    Replace new-style upstream CWs with old-style CWs for now

--- app/javascript/flavours/glitch/components/content_warning.tsx
+++ app/javascript/flavours/glitch/components/content_warning.tsx
@@ -1,15 +1,25 @@
-import { StatusBanner, BannerVariant } from './status_banner';
+/* Significantly rewritten from upstream to keep the old design for now */
+
+import { FormattedMessage } from 'react-intl';
 
 export const ContentWarning: React.FC<{
   text: string;
   expanded?: boolean;
   onClick?: () => void;
 }> = ({ text, expanded, onClick }) => (
-  <StatusBanner
-    expanded={expanded}
-    onClick={onClick}
-    variant={BannerVariant.Yellow}
-  >
-    <p dangerouslySetInnerHTML={{ __html: text }} />
-  </StatusBanner>
+  <p>
+    <span dangerouslySetInnerHTML={{ __html: text }} className='translate' />{' '}
+    <button
+      type='button'
+      className='status__content__spoiler-link'
+      onClick={onClick}
+      aria-expanded={expanded}
+    >
+      {expanded ? (
+        <FormattedMessage id='status.show_less' defaultMessage='Show less' />
+      ) : (
+        <FormattedMessage id='status.show_more' defaultMessage='Show more' />
+      )}
+    </button>
+  </p>
 );
