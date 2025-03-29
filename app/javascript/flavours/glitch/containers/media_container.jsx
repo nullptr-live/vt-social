0a39eac6fe4c15a0a905d94fafc12e214b16af54
Renaud Chaput <renchap@gmail.com>
2025-03-29 21:17:27 +0100

    [Glitch] Convert the polls reducer to plain JS
    Port 1bc28709ccde4106ab7d654ad5888a14c6bb1724 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/containers/media_container.jsx
+++ app/javascript/flavours/glitch/containers/media_container.jsx
@@ -13,6 +13,7 @@ import Card from 'flavours/glitch/features/status/components/card';
 import MediaModal from 'flavours/glitch/features/ui/components/media_modal';
 import { Video } from 'flavours/glitch/features/video';
 import { IntlProvider } from 'flavours/glitch/locales';
+import { createPollFromServerJSON } from 'flavours/glitch/models/poll';
 import { getScrollbarWidth } from 'flavours/glitch/utils/scrollbar';
 
 const MEDIA_COMPONENTS = { MediaGallery, Video, Card, Poll, Hashtag, Audio };
@@ -88,7 +89,7 @@ export default class MediaContainer extends PureComponent {
             Object.assign(props, {
               ...(media   ? { media:   fromJS(media)   } : {}),
               ...(card    ? { card:    fromJS(card)    } : {}),
-              ...(poll    ? { poll:    fromJS(poll)    } : {}),
+              ...(poll    ? { poll:    createPollFromServerJSON(poll)    } : {}),
               ...(hashtag ? { hashtag: fromJS(hashtag) } : {}),
 
               ...(componentName === 'Video' ? {
