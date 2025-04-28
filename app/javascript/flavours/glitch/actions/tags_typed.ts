a43f9423792c522c837100ae6b498c3dd62d6619
Eugen Rochko <eugen@zeonfederated.com>
2025-04-28 13:44:01 +0200

    [Glitch] Add ability to feature and unfeature hashtags from web UI
    Port 40157e063d12b5591a9cccaf92abd783e03586b4 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/actions/tags_typed.ts
+++ app/javascript/flavours/glitch/actions/tags_typed.ts
@@ -2,6 +2,8 @@ import {
   apiGetTag,
   apiFollowTag,
   apiUnfollowTag,
+  apiFeatureTag,
+  apiUnfeatureTag,
 } from 'flavours/glitch/api/tags';
 import { createDataLoadingThunk } from 'flavours/glitch/store/typed_functions';
 
@@ -18,4 +20,14 @@ export const followHashtag = createDataLoadingThunk(
 export const unfollowHashtag = createDataLoadingThunk(
   'tags/unfollow',
   ({ tagId }: { tagId: string }) => apiUnfollowTag(tagId),
+);
+
+export const featureHashtag = createDataLoadingThunk(
+  'tags/feature',
+  ({ tagId }: { tagId: string }) => apiFeatureTag(tagId),
+);
+
+export const unfeatureHashtag = createDataLoadingThunk(
+  'tags/unfeature',
+  ({ tagId }: { tagId: string }) => apiUnfeatureTag(tagId),
 );
