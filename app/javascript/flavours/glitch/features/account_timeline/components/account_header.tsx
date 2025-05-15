9ad3f4ffa8396ce78233e4162389665cb606b489
diondiondion <mail@diondiondion.com>
2025-05-15 13:33:16 +0200

    [Glitch] fix: More "Followers you know" polish & bug fixes
    Port 3f965d83b01c29eadf4e7060a84f10851f6ba5a5 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/features/account_timeline/components/account_header.tsx
+++ app/javascript/flavours/glitch/features/account_timeline/components/account_header.tsx
@@ -916,6 +916,8 @@ export const AccountHeader: React.FC<{
             <div className='account__header__badges'>{badges}</div>
           )}
 
+          {signedIn && <FamiliarFollowers accountId={accountId} />}
+
           {!(suspended || hidden) && (
             <div className='account__header__extra'>
               <div
@@ -993,7 +995,6 @@ export const AccountHeader: React.FC<{
                   ))}
                 </div>
               </div>
-              {signedIn && <FamiliarFollowers accountId={accountId} />}
             </div>
           )}
         </div>
