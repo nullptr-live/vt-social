0f591649c46801c28b953b53aed351f950f3798a
Nicholas La Roux <larouxn@gmail.com>
2025-05-30 21:33:30 +0900

    [Glitch] Make account header profile pic exactly 2x post profile pic
    Port 0789ec93f84ba1bc5cd220cc31622a0779ebe9ca to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/features/account_timeline/components/account_header.tsx
+++ app/javascript/flavours/glitch/features/account_timeline/components/account_header.tsx
@@ -877,7 +877,7 @@ export const AccountHeader: React.FC<{
             >
               <Avatar
                 account={suspended || hidden ? undefined : account}
-                size={90}
+                size={92}
               />
             </a>
 
