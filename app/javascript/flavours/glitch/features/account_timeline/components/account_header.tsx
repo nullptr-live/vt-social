8a8453e3b1cbb7cdf2d99b6204c85487a4acd887
Echo <ChaosExAnima@users.noreply.github.com>
2025-07-22 16:43:15 +0200

    [Glitch] Emoji Rendering
    Port 760d00b7f7491911a43ffc5bf6e8ce709c9da000 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/features/account_timeline/components/account_header.tsx
+++ app/javascript/flavours/glitch/features/account_timeline/components/account_header.tsx
@@ -902,8 +902,7 @@ export const AccountHeader: React.FC<{
                 )}
 
                 <AccountBio
-                  note={account.note_emojified}
-                  dropdownAccountId={accountId}
+                  accountId={accountId}
                   className='account__header__content'
                 />
 
