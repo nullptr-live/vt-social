a8432560ba34d8b56932c3c70f8b08d74bce1e6d
Eugen Rochko <eugen@zeonfederated.com>
2025-07-23 15:42:07 +0200

    [Glitch] Add button to load new replies in web UI
    Port 14a781fa24c969a6be4f2ccc3e6e5c9f83db7437 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/actions/statuses_typed.ts
+++ app/javascript/flavours/glitch/actions/statuses_typed.ts
@@ -1,3 +1,5 @@
+import { createAction } from '@reduxjs/toolkit';
+
 import { apiGetContext } from 'flavours/glitch/api/statuses';
 import { createDataLoadingThunk } from 'flavours/glitch/store/typed_functions';
 
@@ -6,13 +8,18 @@ import { importFetchedStatuses } from './importer';
 export const fetchContext = createDataLoadingThunk(
   'status/context',
   ({ statusId }: { statusId: string }) => apiGetContext(statusId),
-  (context, { dispatch }) => {
+  ({ context, refresh }, { dispatch }) => {
     const statuses = context.ancestors.concat(context.descendants);
 
     dispatch(importFetchedStatuses(statuses));
 
     return {
       context,
+      refresh,
     };
   },
+);
+
+export const completeContextRefresh = createAction<{ statusId: string }>(
+  'status/context/complete',
 );
