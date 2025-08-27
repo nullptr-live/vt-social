9c662f83d22d37c44b72cd9aae1eaeeeeae0fbe0
Claire <claire.github-309c@sitedethib.com>
2025-08-27 10:13:34 +0200

    [Glitch] Fix API return types for interaction API helpers
    Port 66f5ad42e2a4c779eddd8d9c30030e29ea129e60 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/api/interactions.ts
+++ app/javascript/flavours/glitch/api/interactions.ts
@@ -1,15 +1,16 @@
 import { apiRequestPost } from 'flavours/glitch/api';
-import type { Status, StatusVisibility } from 'flavours/glitch/models/status';
+import type { ApiStatusJSON } from 'flavours/glitch/api_types/statuses';
+import type { StatusVisibility } from 'flavours/glitch/models/status';
 
 export const apiReblog = (statusId: string, visibility: StatusVisibility) =>
-  apiRequestPost<{ reblog: Status }>(`v1/statuses/${statusId}/reblog`, {
+  apiRequestPost<{ reblog: ApiStatusJSON }>(`v1/statuses/${statusId}/reblog`, {
     visibility,
   });
 
 export const apiUnreblog = (statusId: string) =>
-  apiRequestPost<Status>(`v1/statuses/${statusId}/unreblog`);
+  apiRequestPost<ApiStatusJSON>(`v1/statuses/${statusId}/unreblog`);
 
 export const apiRevokeQuote = (quotedStatusId: string, statusId: string) =>
-  apiRequestPost<Status>(
+  apiRequestPost<ApiStatusJSON>(
     `v1/statuses/${quotedStatusId}/quotes/${statusId}/revoke`,
   );
