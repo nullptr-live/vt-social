50acfc48b989f31eccfe747131809c923fbf04b8
Echo <ChaosExAnima@users.noreply.github.com>
2025-09-08 14:35:59 +0200

    [Glitch] Remove old search results with new search
    Port 24fb862a65da2545c439ae33625b5a049343dc80 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/reducers/search.ts
+++ app/javascript/flavours/glitch/reducers/search.ts
@@ -65,6 +65,9 @@ export const searchReducer = createReducer(initialState, (builder) => {
     (state, action) => {
       state.type = action.meta.arg.type;
       state.loading = true;
+      if (action.type === submitSearch.pending.type) {
+        state.results = undefined;
+      }
     },
   );
 
