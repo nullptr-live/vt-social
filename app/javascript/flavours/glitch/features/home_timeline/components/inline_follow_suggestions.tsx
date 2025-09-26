a4dc785de326ce60402952e05dc66fb76583d9c4
diondiondion <mail@diondiondion.com>
2025-09-26 12:00:50 +0200

    [Glitch] Update "Follow" button labels
    Port cb5bbbfb051e175afb538bf9b83a7ca2d2b2b867 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/features/home_timeline/components/inline_follow_suggestions.tsx
+++ app/javascript/flavours/glitch/features/home_timeline/components/inline_follow_suggestions.tsx
@@ -25,8 +25,6 @@ import { domain } from 'flavours/glitch/initial_state';
 import { useAppDispatch, useAppSelector } from 'flavours/glitch/store';
 
 const messages = defineMessages({
-  follow: { id: 'account.follow', defaultMessage: 'Follow' },
-  unfollow: { id: 'account.unfollow', defaultMessage: 'Unfollow' },
   previous: { id: 'lightbox.previous', defaultMessage: 'Previous' },
   next: { id: 'lightbox.next', defaultMessage: 'Next' },
   dismiss: {
