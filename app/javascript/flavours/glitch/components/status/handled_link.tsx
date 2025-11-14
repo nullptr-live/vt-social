7141917943fac3ab65e5535fb7b41cc943d48e1e
Echo <ChaosExAnima@users.noreply.github.com>
2025-11-14 15:11:41 +0100

    [Glitch] Fix error with remote tags including percent signs
    Port 6486c092f631c80603e71c1fc83cb341f6855a8e to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/components/status/handled_link.tsx
+++ app/javascript/flavours/glitch/components/status/handled_link.tsx
@@ -133,12 +133,14 @@ export const HandledLink: FC<HandledLinkProps & ComponentProps<'a'>> = ({
 
   // Handle hashtags
   if (
-    text.startsWith('#') ||
-    prevText?.endsWith('#') ||
-    text.startsWith('＃') ||
-    prevText?.endsWith('＃')
+    (text.startsWith('#') ||
+      prevText?.endsWith('#') ||
+      text.startsWith('＃') ||
+      prevText?.endsWith('＃')) &&
+    !text.includes('%')
   ) {
     const hashtag = text.slice(1).trim();
+
     return (
       <Link
         className={classNames('mention hashtag', className)}
@@ -192,7 +194,7 @@ export const HandledLink: FC<HandledLinkProps & ComponentProps<'a'>> = ({
   return (
     <a
       {...props}
-      href={href}
+      href={encodeURI(href)}
       title={href}
       className={classNames('unhandled-link', className)}
       target='_blank'
